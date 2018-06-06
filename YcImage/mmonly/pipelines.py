b0afdfc9f90ab4ecf4341e100274287062cc6b4e
yinchong <39363845+chy19891014@users.noreply.github.com>
2018-06-06 14:16:41 +0800

    Update pipelines.py

--- YcImage/mmonly/pipelines.py
+++ YcImage/mmonly/pipelines.py
@@ -1,29 +1,24 @@
 # -*- coding: utf-8 -*-
 import requests
-import sys
-import threading
-reload(sys)
-sys.setdefaultencoding('utf-8')
+import datetime
 
 class mmonlyPipeline(object):
     def process_item(self, item, spider):
         count = 0
         detailURL = item['detailURL']
-        path = item['path']
         fileName = item['fileName']
         while True:
             try:
-                print u'正在保存图片：', detailURL
-                print u'图片路径：', path
+                print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), u'正在保存图片：', detailURL
                 print u'文件：', fileName
                 image = requests.get(detailURL)
-                f = open(path, 'wb')
+                f = open(fileName, 'wb')
                 f.write(image.content)
                 f.close()
-                return item
             except Exception, e:
                 print fileName, 'other fault:', e
                 count += 1
             else:
-                print u'图片保存成功！'
+                print datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), fileName, u'保存成功！'
                 break
+        return item

bc02c2f21426fde81f34c20116938e4305cc4a68
yinchong <39363845+chy19891014@users.noreply.github.com>
2018-06-10 01:30:15 +0800

    Update settings.py

--- YcImage/mmonly/settings.py
+++ YcImage/mmonly/settings.py
@@ -6,6 +6,20 @@ SPIDER_MODULES = ['mmonly.spiders']
 NEWSPIDER_MODULE = 'mmonly.spiders'
 FEED_EXPORT_ENCODING = 'utf-8'
 
+######################################## scrapy-redis配置#################
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+# 使用scrapy-redis里的去重组件，不使用scrapy默认的去重方式，在redis数据库里做去重。
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+# 使用scrapy-redis里的调度器组件，不使用默认的调度器
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+# 允许暂停，redis请求记录不丢失，在redis queues 允许暂停和暂停后恢复，也就是不清理redis queues
+SCHEDULER_PERSIST = True
+# 默认的scrapy-redis请求队列形式（按优先级）
+SCHEDULER_QUEUE_CLASS = "scrapy_redis.queue.SpiderPriorityQueue"
+######################################## scrapy-redis配置#################
+
 ROBOTSTXT_OBEY = False
 #CONCURRENT_ITEMS = 200
 # 默认是16，一次可以请求的最大次数
@@ -21,10 +35,13 @@ DEFAULT_REQUEST_HEADERS = {
     'Connection':'keep-alive',
     'User-Agent':'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'}
 
-ITEM_PIPELINES = {'mmonly.pipelines.mmonlyPipeline': 100}
+ITEM_PIPELINES = {
+    'mmonly.pipelines.mmonlyPipeline': 300,
+    'scrapy_redis.pipelines.RedisPipeline': 400,
+}
 
-HTTPCACHE_ENABLED = True
-HTTPCACHE_EXPIRATION_SECS = 0
-HTTPCACHE_DIR = 'httpcache'
-HTTPCACHE_IGNORE_HTTP_CODES = []
-HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
